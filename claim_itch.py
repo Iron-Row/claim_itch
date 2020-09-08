2b39e7c5dda9589a86a7438d45c0d43f1c59b893
tauqua <bsimon@oberlin.edu>
2020-09-08 14:44:59 -0500

    Don't check sale games if sale is ended

--- claim_itch.py
+++ claim_itch.py
@@ -119,7 +119,11 @@ def extract_from_itch_group(group_page):
     OUTPUT urls of all games, urls of games that avie noted is connected to more sales
     '''
     soup = BeautifulSoup(group_page, 'lxml')
+    ended = soup.find_all('div', class_ = 'not_active_notification')
     urls, more = set(), set()
+    if ended:
+        print(" Sale ended")
+        return urls, more
     games = soup.find_all('div', class_='game_cell')
     for game in games:
         url = game.find('a').get('href')
